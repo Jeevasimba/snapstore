a7a03446d6710c6e25c43945c3126704133761d0
Jeevasimba <96039278+Jeevasimba@users.noreply.github.com>
2022-10-12 16:10:04 +0530

    Update Controller.java

--- src/main/java/org/example/hellofx/Controller.java
+++ src/main/java/org/example/hellofx/Controller.java
@@ -4,13 +4,5 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
-    @FXML Label helloLabel;
 
-    String[] greetings = {"Hello", "I", "am", "pradeep"};
-    int currentGreetingIndex = 0;
-
-    public void changeGreeting() {
-        currentGreetingIndex = (currentGreetingIndex + 1) % greetings.length;
-        helloLabel.setText(greetings[currentGreetingIndex]);
-    }
 }
